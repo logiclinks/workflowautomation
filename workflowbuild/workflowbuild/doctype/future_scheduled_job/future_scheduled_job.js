758ba402cb2db9dae9a29a6404ed5a6bec81a704
logiclinks <reed@elevationmtg.com>
2025-06-24 04:22:04 +0530

    Add: future_scheduled_jobs

---
+++ workflowbuild/workflowbuild/doctype/future_scheduled_job/future_scheduled_job.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Mradul Mishra, Shubhamm Mohitte and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Future Scheduled Job", {
+// 	refresh(frm) {
+
+// 	},
+// });
